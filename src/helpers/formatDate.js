0162efde5aaa0e1a0ca02aa7ab006ac2254756f5
DmytroKaniuchok <kanyuchekdmitry@gmail.com>
2025-04-26 14:53:43 +0300

    finish

--- src/helpers/formatDate.js
+++ src/helpers/formatDate.js
@@ -1,4 +1,9 @@
-import { formatDistanceToNow } from 'date-fns';
+import { formatDistanceToNow, formatDate } from 'date-fns';
+
 export const formatDataNow = data => {
   return formatDistanceToNow(new Date(data), { addSuffix: true });
+};
+
+export const dateTransactions = date => {
+  return formatDate(new Date(date), 'Pp');
 };
